f9de75e6c416a1299fc2a675486b4a06f902da9c
SumaanNisar <beighsumaan@gmail.com>
2025-01-28 14:29:47 +0530

    Hero Section

--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Hero from "../../components/student/Hero";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="felx flex-col items center space-y-7 text-center">
+      <Hero />
     </div>
   );
 };
